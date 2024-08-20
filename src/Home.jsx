25c3590e2be66ae1c2eae4b582d767df469de022
Gabe Harvey <harvey7@gmail.com>
2024-08-20 13:59:40 -0500

    Component Test

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,18 @@ function Paragraph () {
     )
 }
 
+function Segment () {
+    return (
+        <div>
+            <ol>
+                <li>Test 1</li>
+                <li>Test 2</li>
+                <li>Test 3</li>
+            </ol>
+        </div>
+    )
+}
+
 function Home () {
     return (
         <div>
@@ -16,6 +28,7 @@ function Home () {
                 <li>I enjoy learning React because it was invented by Facebook and actively maintained.</li>
                 <li>I enjoy learning React because it is a composable framework</li>
             </ul>
+            <Segment />
         </div>
     )
 }
