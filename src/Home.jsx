a1278ed17af74b10eaa2c65c22eed5a640fcf448
Gabe Harvey <harvey7@gmail.com>
2024-08-20 14:40:22 -0500

    Code cleanup throughout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import "./App.css";
 
-function Test () {
-    return (
-        <p>This is another test</p>
-    )
-}
-
 function Home () {
     return (
         <div>
-            <Test />
             <h1 className='main-header'>This is Gabe's React Page</h1>
-            <ul>
+            <ul className="main-list">
                 <li>I enjoy learning React because it is a hirable skill.</li>
                 <li>I enjoy learning React because it was invented by Facebook and actively maintained.</li>
                 <li>I enjoy learning React because it is a composable framework</li>
