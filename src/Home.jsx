b24f3b1ef4fe69b46bf2758151632407524a9f4a
Gabe Harvey <harvey7@gmail.com>
2024-08-20 14:36:40 -0500

    Added google font to index.html

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ function Home () {
     return (
         <div>
             <Test />
-            <h1>This is Gabe's React Page</h1>
+            <h1 className='main-header'>This is Gabe's React Page</h1>
             <ul>
                 <li>I enjoy learning React because it is a hirable skill.</li>
                 <li>I enjoy learning React because it was invented by Facebook and actively maintained.</li>
