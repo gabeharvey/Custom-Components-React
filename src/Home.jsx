a5b5889bfef064bfd783e079ffd63bbeefc15307
Gabe Harvey <harvey7@gmail.com>
2024-08-20 13:58:06 -0500

    Component Test

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+function Paragraph () {
+    return (
+        <p>This is a Test</p>
+    )
+}
+
 function Home () {
     return (
         <div>
+            <Paragraph />
             <h1>This is Gabe's React Page</h1>
             <ul>
                 <li>I enjoy learning React because it is a hirable skill.</li>
