fe45ea87eae9988e73d9c487a0557a0e308904ad
Gabe Harvey <harvey7@gmail.com>
2024-08-20 13:17:29 -0500

    Challenge 1 components complete

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home () {
+    return (
+        <div>
+            <ul>
+                <li>I enjoy learning React because it is a hirable skill.</li>
+                <li>I enjoy learning React because it was invented by Facebook and actively maintained.</li>
+                <li>I enjoy learning React because it is a composable framework</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
