b806eaf174734794b1517302e684c54eb409c2eb
Gabe Harvey <harvey7@gmail.com>
2024-08-20 13:29:58 -0500

    Challenge 2 components complete

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import logo from "./logo.svg"
 
 function Home () {
     return (
         <div>
+            <header>
+                <nav>
+                    <img src={logo} alt="logo" width={80}></img>
+                </nav>
+            </header>
+            <h1>This is Gabe's React Page</h1>
             <ul>
                 <li>I enjoy learning React because it is a hirable skill.</li>
                 <li>I enjoy learning React because it was invented by Facebook and actively maintained.</li>
                 <li>I enjoy learning React because it is a composable framework</li>
             </ul>
+            <footer>
+                ©2024 Harvey Development. All rights reserved.
+            </footer>
         </div>
     )
 }
