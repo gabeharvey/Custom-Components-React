a2745bb1f9a61d0d61c043f8a6808429a7105d87
Gabe Harvey <harvey7@gmail.com>
2024-08-20 14:21:36 -0500

    Code cleanup

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,28 +7,9 @@ function Test () {
     )
 }
 
-function Paragraph () {
-    return (
-        <p>This is a Test</p>
-    )
-}
-
-function Segment () {
-    return (
-        <div>
-            <ol className="nav-items">
-                <li>Test 1</li>
-                <li>Test 2</li>
-                <li>Test 3</li>
-            </ol>
-        </div>
-    )
-}
-
 function Home () {
     return (
         <div>
-            <Paragraph />
             <Test />
             <h1>This is Gabe's React Page</h1>
             <ul>
@@ -36,7 +17,6 @@ function Home () {
                 <li>I enjoy learning React because it was invented by Facebook and actively maintained.</li>
                 <li>I enjoy learning React because it is a composable framework</li>
             </ul>
-            <Segment />
         </div>
     )
 }
